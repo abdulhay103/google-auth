87ab59eff29b662379f6f1bd3cae56666f8eae4f
abdulhay103 <abdulhay.me@gmail.com>
2023-09-13 11:23:55 +0600

    setup

--- src/app/utils/MiddlewareHelper.js
+++ src/app/utils/MiddlewareHelper.js
@@ -3,12 +3,15 @@ import { VerifyToken } from "./JWTokenKit";
 
 export async function CheckAuth(req) {
   try {
-    let token = req.cookies.get("token");
-    let payload = await VerifyToken(token["value"]);
-    console.log(payload["email"]);
-
     const reqHeaders = new Headers(req.headers);
-    reqHeaders.set("email", payload["email"]);
+    let token = req.cookies.get("token");
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+    let payload = await VerifyToken(token["value"]);
+    let email = payload["email"];
+    console.log(email);
+    reqHeaders.set("email", email);
     return NextResponse.next({ request: { headers: reqHeaders } });
   } catch (error) {
     return NextResponse.redirect(new URL("/login", req.url));
