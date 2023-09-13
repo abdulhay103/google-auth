550421ac89ab54a6cc0047ae323956e580f6b7b4
abdulhay103 <abdulhay.me@gmail.com>
2023-09-13 14:43:25 +0600

    session

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,21 +1,40 @@
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
+  const { data, status } = useSession();
   return (
     <header className=" w-full shadow bg-white">
       <nav className="container mx-auto flex justify-between py-4">
         <div>
           <Link href="/">Home</Link>
         </div>
-        <ul className="flex gap-6">
-          <li>
-            <Link href="/login">Login</Link>
-          </li>
-          <li>
-            <Link href="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
+        {status === "authenticated" ? (
+          <ul className="flex gap-6">
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => {
+                  signOut({ callbackUrl: "/login" });
+                }}
+              >
+                Logout
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <ul className="flex gap-6">
+            <li>
+              <Link href="/login">Login</Link>
+            </li>
+            <li>
+              <Link href="/register">Register</Link>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
